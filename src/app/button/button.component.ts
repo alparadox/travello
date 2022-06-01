5bb8e68337bae8083f79b565923921b2a3fd1926
Alyona <alyona.dux@yandex.ru>
2022-06-01 14:32:53 +0300

    добавлен компонент button

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() title = 'button';
+  @Input() type = 's';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
