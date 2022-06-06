beda690a82c3f5724875e017ac714ae9b07c8be8
Alyona <alyona.dux@yandex.ru>
2022-06-06 11:39:37 +0300

    компонент plan - добавлен input в ts

--- src/app/plan/plan.component.ts
+++ src/app/plan/plan.component.ts
@@ -8,6 +8,13 @@ import {Component, Input, OnInit} from '@angular/core';
 export class PlanComponent implements OnInit {
 
   @Input() titleButton = 'title';
+  @Input() price = 39;
+  @Input() rate = 'Personal';
+  @Input() description = 'description for cards with tariff plan on the site';
+  @Input() itemOne = 'Secure your customer usage';
+  @Input() itemTwo = 'View basic analytics';
+  @Input() itemFree = 'Up to 350 customer profiles';
+  @Input() itemFour = 'Custom network name';
 
   constructor() { }
 
